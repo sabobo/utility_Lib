64706fa0b7f324b10d0ddbd861ee5ae72af65c72
sabobo <906621525@qq.com>
2015-06-24 10:49:16 +0800

    lib
    lib引用包

---
+++ utility_lib/src/net/dev/mylib/numberpicker/NumberPickerDemo.java
@@ -0,0 +1,40 @@
+/**
+ * 模仿时间选择器的拨片式选择，
+ * 在使用这个拨片选择器的时候需要更改使用的主题样式，否则无法使用
+ * android:theme="@style/SampleTheme.Light"
+ * android:theme="@style/SampleTheme"
+ * 
+ */
+//package net.dev.mylib.numberpicker;
+//
+//import net.dev.mylib.R;
+//import net.dev.mylib.numberpicker.NumberPicker;
+//import android.app.Activity;
+//import android.os.Bundle;
+//
+//public class NumberPickerDemo extends Activity {
+//
+//	@Override
+//	protected void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_main);
+//		NumberPicker np = (NumberPicker) findViewById(R.id.numberPicker);
+//		np.setMaxValue(20);
+//		np.setMinValue(0);
+//		np.setFocusable(true);
+//		np.setFocusableInTouchMode(true);
+//	}
+//
+//}
+/**布局文件使用方式*/
+//<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+//    xmlns:tools="http://schemas.android.com/tools"
+//    android:layout_width="match_parent"
+//    android:layout_height="match_parent" >
+//
+//     <net.dev.mylib.numberpicker.NumberPicker
+//        android:id="@+id/numberPicker"
+//        android:layout_width="wrap_content"
+//        android:layout_height="wrap_content" />
+//
+// </RelativeLayout>
